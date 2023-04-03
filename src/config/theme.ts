b38f73717913dea4d7c6a9573956c7cf500ec449
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2023-04-03 15:43:51 +0700

    create theme and palette

---
+++ src/config/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#2FCC59"
+        },
+        background: {
+            default: '#E9E9EB',
+            paper: '#D9D9D9',
+        },
+    }
+})
+
+export default theme
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import theme from '@/config/theme'
+import { CssBaseline, GlobalStyles, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
