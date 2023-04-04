f541ece479137df06cba4e6f3200acd973aa0578
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2023-04-04 19:44:23 +0700

    Collapse for 1-1 and group chat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,31 +2,53 @@ import Chat from "@/component/chat/Chat";
 import NavBar from "@/component/navbar/NavBar";
 import CreateGroup from "@/component/group/CreateGroup";
 import CenterList from "@/component/chat/CenterList";
+import { Collapse, ListItemButton, Typography } from "@mui/material";
+import { useState } from "react";
+import theme from "@/config/theme";
 
 export default function Home() {
+  const [openCollapseClient, setOpenCollapseClient] = useState<boolean>(false)
+  const [openCollapseServer, setOpenCollapseServer] = useState<boolean>(false)
+
+  function onClickCollapseClient(): void {
+    setOpenCollapseClient(!openCollapseClient)
+  }
+
+  function onClickCollapseServer(): void {
+    setOpenCollapseServer(!openCollapseServer)
+  }
+
   return (
     <>
       <NavBar />
       <CenterList>
-        <Chat href="/chat/1" label="client1" type="1-1" />
-        <Chat href="/chat/1" label="client1" type="1-1" />
-        <Chat href="/chat/1" label="client1" type="1-1" />
-        <Chat href="/chat/1" label="client1" type="1-1" />
-        <Chat href="/chat/1" label="client1" type="1-1" />
-      </CenterList>
-      <CenterList>
-        <Chat href="/group_chat/1" label="client1" type="Group" />
-        <Chat href="/group_chat/1" label="client1" type="Group" />
-        <Chat href="/group_chat/1" label="client1" type="Group" />
-        <Chat href="/group_chat/1" label="client1" type="Group" />
-        <Chat href="/group_chat/1" label="client1" type="Group" />
-        <Chat href="/group_chat/1" label="client1" type="Group" />
-        <Chat href="/group_chat/1" label="client1" type="Group" />
-        <Chat href="/group_chat/1" label="client1" type="Group" />
-        <Chat href="/group_chat/1" label="client1" type="Group" />
-        <Chat href="/group_chat/1" label="client1" type="Group" />
-        <Chat href="/group_chat/1" label="client1" type="Group" />
-      </CenterList>
+        <ListItemButton sx={{ minHeight: "56px", backgroundColor: theme.palette.primary.main }} onClick={onClickCollapseClient}>
+          <Typography>Clients ()</Typography>
+        </ListItemButton>
+        <Collapse in={openCollapseClient}>
+          <Chat href="/chat/1" label="client1" type="1-1" />
+          <Chat href="/chat/1" label="client1" type="1-1" />
+          <Chat href="/chat/1" label="client1" type="1-1" />
+          <Chat href="/chat/1" label="client1" type="1-1" />
+          <Chat href="/chat/1" label="client1" type="1-1" />
+        </Collapse >
+        <ListItemButton sx={{ minHeight: "56px", backgroundColor: theme.palette.primary.main }} onClick={onClickCollapseServer}>
+          <Typography>Groups ()</Typography>
+        </ListItemButton>
+        <Collapse in={openCollapseServer}>
+          <Chat href="/group_chat/1" label="client1" type="Group" />
+          <Chat href="/group_chat/1" label="client1" type="Group" />
+          <Chat href="/group_chat/1" label="client1" type="Group" />
+          <Chat href="/group_chat/1" label="client1" type="Group" />
+          <Chat href="/group_chat/1" label="client1" type="Group" />
+          <Chat href="/group_chat/1" label="client1" type="Group" />
+          <Chat href="/group_chat/1" label="client1" type="Group" />
+          <Chat href="/group_chat/1" label="client1" type="Group" />
+          <Chat href="/group_chat/1" label="client1" type="Group" />
+          <Chat href="/group_chat/1" label="client1" type="Group" />
+          <Chat href="/group_chat/1" label="client1" type="Group" />
+        </Collapse>
+      </CenterList >
       <CreateGroup />
     </>
   )
