8d0ac038947e2fee23e7807ec45309b9f5c85945
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2023-04-04 20:36:25 +0700

    Implement useCollaspe.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,39 +3,31 @@ import NavBar from "@/component/navbar/NavBar";
 import CreateGroup from "@/component/group/CreateGroup";
 import CenterList from "@/component/chat/CenterList";
 import { Collapse, ListItemButton, Typography } from "@mui/material";
-import { useState } from "react";
 import theme from "@/config/theme";
+import useCollaspe from "@/hook/useCollaspe";
 
 export default function Home() {
-  const [openCollapseClient, setOpenCollapseClient] = useState<boolean>(false)
-  const [openCollapseServer, setOpenCollapseServer] = useState<boolean>(false)
-
-  function onClickCollapseClient(): void {
-    setOpenCollapseClient(!openCollapseClient)
-  }
-
-  function onClickCollapseServer(): void {
-    setOpenCollapseServer(!openCollapseServer)
-  }
+  const collaspeClient = useCollaspe()
+  const collaspeServer = useCollaspe()
 
   return (
     <>
       <NavBar />
       <CenterList>
-        <ListItemButton sx={{ minHeight: "56px", backgroundColor: theme.palette.primary.main }} onClick={onClickCollapseClient}>
-          <Typography>Clients ()</Typography>
+        <ListItemButton sx={{ minHeight: "56px", backgroundColor: theme.palette.primary.main }} onClick={collaspeClient.onClick}>
+          <Typography>Clients (0)</Typography>
         </ListItemButton>
-        <Collapse in={openCollapseClient}>
+        <Collapse in={collaspeClient.open}>
           <Chat href="/chat/1" label="client1" type="1-1" />
           <Chat href="/chat/1" label="client1" type="1-1" />
           <Chat href="/chat/1" label="client1" type="1-1" />
           <Chat href="/chat/1" label="client1" type="1-1" />
           <Chat href="/chat/1" label="client1" type="1-1" />
         </Collapse >
-        <ListItemButton sx={{ minHeight: "56px", backgroundColor: theme.palette.primary.main }} onClick={onClickCollapseServer}>
-          <Typography>Groups ()</Typography>
+        <ListItemButton sx={{ minHeight: "56px", backgroundColor: theme.palette.primary.main }} onClick={collaspeServer.onClick}>
+          <Typography>Groups (0)</Typography>
         </ListItemButton>
-        <Collapse in={openCollapseServer}>
+        <Collapse in={collaspeServer.open}>
           <Chat href="/group_chat/1" label="client1" type="Group" />
           <Chat href="/group_chat/1" label="client1" type="Group" />
           <Chat href="/group_chat/1" label="client1" type="Group" />
