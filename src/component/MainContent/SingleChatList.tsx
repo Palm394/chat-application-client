ddb8abdc25fd2ef3e987eafe24418d9a5aa2db72
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2023-04-04 14:29:49 +0700

    Fix type

--- src/component/MainContent/SingleChatList.tsx
+++ src/component/MainContent/SingleChatList.tsx
@@ -1,16 +1,17 @@
 import { Avatar, ListItemButton, Typography } from "@mui/material";
 import MessageIcon from '@mui/icons-material/Message';
+import { ChatType } from "@/type/Chat";
 
 type props = {
   href: string,
   label: string,
-  type: "Client" | "Group"
+  type: ChatType
 }
 
 export default function SingleChatList({ ...props }: props) {
   return (
     <ListItemButton href={props.href} sx={{ minHeight: "56px", margin: 0 }}>
-      {props.type === "Client" &&
+      {props.type === "1-1" &&
         <Avatar sx={{ marginRight: "3vw" }} />
       }
       <Typography sx={{ flexGrow: 1 }}>{props.label}</Typography>
