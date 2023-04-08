08defc9bf074b51291e27817be10979675f5dfa0
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2023-04-08 13:19:47 +0700

    Cancel to use layout abstract with index navbar

--- src/component/NavBar/NavBar.tsx
+++ src/component/NavBar/NavBar.tsx
@@ -1,13 +1,17 @@
-import { Box, IconButton, TextField, Typography } from "@mui/material"
+import { AppBar, Avatar, Box, IconButton, TextField, Toolbar, Typography } from "@mui/material";
+import { useState } from "react";
 
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
 import EditIcon from '@mui/icons-material/Edit';
 
-import NavBarLayout from "./NavBarLayout";
-import { useState } from "react";
+type props = {
+    avatar?: string,
+    displayComponent: JSX.Element,
+    children?: JSX.Element
+}
 
-export default function NavBar() {
+export default function NavBarIndex({ ...props }: props) {
     const [isEditMode, setIsEditMode] = useState<boolean>(false)
     const [newName, setNewName] = useState<string>("")
     function openEditMode(): void {
@@ -16,28 +20,37 @@ export default function NavBar() {
     function closeEditMode(): void {
         setIsEditMode(false)
     }
-
     return (
-        <NavBarLayout avatar="n"
-            displayComponent={
-                isEditMode ?
-                    <TextField
-                        helperText={`${newName.length}/20`}
-                        onChange={(e) => setNewName(e.target.value)}
-                    />
-                    :
-                    <Typography>{"client1"}</Typography>
-            }>
-            {isEditMode ?
-                <Box>
-                    <IconButton onClick={closeEditMode}><CloseIcon /></IconButton>
-                    <IconButton><CheckIcon /></IconButton>
+        <AppBar
+            position="sticky"
+            sx={{
+                borderBottom: "2px solid #000000"
+            }}
+        >
+            <Toolbar>
+                <Avatar sx={{ marginRight: "3vw" }}>{props.avatar}</Avatar>
+                <Box sx={{ flexGrow: 1 }}>
+                    {isEditMode ?
+                        <TextField
+                            onChange={e => setNewName(e.target.value)}
+                            helperText={`${newName.length}/20`}
+                            autoFocus
+                        />
+                        :
+                        <Typography> Username</Typography>
+                    }
                 </Box>
-                :
-                <IconButton onClick={openEditMode}>
-                    <EditIcon />
-                </IconButton>
-            }
-        </NavBarLayout>
+                {isEditMode ?
+                    <Box>
+                        <IconButton onClick={closeEditMode}><CloseIcon /></IconButton>
+                        <IconButton><CheckIcon /></IconButton>
+                    </Box>
+                    :
+                    <IconButton onClick={openEditMode}>
+                        <EditIcon />
+                    </IconButton>
+                }
+            </Toolbar>
+        </AppBar>
     )
 }
