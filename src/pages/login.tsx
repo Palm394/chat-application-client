5afd1ed0dd0a67ead214a561c2467ffc865dc979
Chinnapat.N <69895436+Kkr1328@users.noreply.github.com>
2023-04-11 17:29:52 +0700

    Implement login service

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,21 +1,55 @@
-import theme from "@/config/theme"
-import { Button, Stack, TextField, Typography } from "@mui/material"
+import theme from "@/config/theme";
+import { SocketContext } from "@/context/SocketContext";
+import { UserContext } from "@/context/UserContext";
+import { LoginResType } from "@/type/Socket";
+import { Button, Stack, TextField, Typography } from "@mui/material";
+import { useRouter } from "next/router";
+import { useContext, useEffect, useState } from "react";
 
 export default function Login() {
-  function onSubmit(): void {
+  const router = useRouter();
+  const socket = useContext(SocketContext);
+  const userContext = useContext(UserContext);
 
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [response, setResponse] = useState<LoginResType | null>(null);
+
+  // open port to receive response from backend service
+  socket.on("login_response", (res: LoginResType) => {
+    setResponse(res);
+    console.log(res.message);
+  });
+
+  function onSubmit(): void {
+    socket.emit("login", { username: username, password: password });
+    return;
   }
+
+  useEffect(() => {
+    if (userContext && response && response.userId) {
+      console.log(response);
+      if (response.message === "Success") {
+        userContext.setUser({ username: username, user_id: response.userId });
+        socket.off("login_response");
+        router.push("/");
+        return;
+      }
+    }
+  }, [socket, response]);
 
   return (
     <>
       <Typography>Login</Typography>
       <Stack sx={{ width: "300px" }} spacing={theme.spacing(2)}>
         <Typography>Username</Typography>
-        <TextField />
+        <TextField value={username} onChange={(event) => setUsername(event.target.value)} />
         <Typography>Password</Typography>
-        <TextField />
-        <Button onClick={onSubmit} variant="contained">Login</Button>
+        <TextField value={password} onChange={(event) => setPassword(event.target.value)} />
+        <Button onClick={onSubmit} variant="contained">
+          Login
+        </Button>
       </Stack>
     </>
-  )
+  );
 }
