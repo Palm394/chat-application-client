79d60ea32362447fd11036bcee64ac385311dc14
Chinnapat.N <69895436+Kkr1328@users.noreply.github.com>
2023-04-11 23:24:05 +0700

    Add message service for group chat

--- src/pages/group_chat/[group_id].tsx
+++ src/pages/group_chat/[group_id].tsx
@@ -2,25 +2,52 @@ import ChatBox from "@/component/chat/ChatBox";
 import CenterList from "@/component/chat/CenterList";
 import Message from "@/component/chat/message/Message";
 import NavBar from "@/component/navbar/NavBarGroup";
+import { useContext, useEffect, useState } from "react";
+import { SocketContext } from "@/context/SocketContext";
+import { useRouter } from "next/router";
 
 export default function GroupChat() {
+  const router = useRouter();
+  const socket = useContext(SocketContext);
+
+  const chatId = router.query.group_id?.toString().replaceAll('"', "");
+  const [messages, setMessages] = useState<any>({});
+
+  useEffect(() => {
+    const messageListener = (message: any) => {
+      setMessages((prevMessages: any) => {
+        const newMessages = { ...prevMessages };
+        newMessages[message._id] = message;
+        return newMessages;
+      });
+    };
+
+    const indentifier = {
+      type: "Group",
+      ownerId: "64339a88ede9b0fc8a482d45",
+      chatId: chatId,
+    };
+    socket.on("get_messages_response", (res: any) => console.log(res.message));
+    socket.on("message", messageListener);
+    socket.emit("getMessages", indentifier);
+  }, [socket]);
+
   return (
     <>
       <NavBar />
       <CenterList>
-        <Message text={"sample message"} isMine={false} type={"Group"} />
-        <Message text={"sample message"} isMine={false} type={"Group"} />
-        <Message text={"sample message"} isMine={false} type={"Group"} />
-        <Message text={"sample message"} isMine={false} type={"Group"} />
-        <Message text={"sample message"} isMine={true} type={"Group"} />
-        <Message text={"sample message"} isMine={false} type={"Group"} />
-        <Message text={"sample message"} isMine={false} type={"Group"} />
-        <Message text={"sample message"} isMine={false} type={"Group"} />
-        <Message text={"sample message"} isMine={false} type={"Group"} />
-        <Message text={"sample message"} isMine={true} type={"Group"} />
-        <Message text={"sample message"} isMine={true} type={"Group"} />
+        {[...Object.values(messages)]
+          .sort((a: any, b: any) => a.createdAt - b.createdAt)
+          .map((message: any) => (
+            <Message
+              key={message._id}
+              text={message.message}
+              isMine={message.isOwner}
+              type={"Group"}
+            />
+          ))}
       </CenterList>
       <ChatBox />
     </>
-  )
+  );
 }
