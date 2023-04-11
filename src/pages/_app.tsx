a9549494b6d9559d517c8fbe46e4b8a4e3cc9bcb
Chinnapat.N <69895436+Kkr1328@users.noreply.github.com>
2023-04-11 16:56:51 +0700

    Add socket context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,27 @@
-import theme from '@/config/theme'
-import { UserContext } from '@/context/UserContext'
-import { User } from '@/type/User'
-import { Box, CssBaseline, ThemeProvider } from '@mui/material'
-import type { AppProps } from 'next/app'
-import { useState } from 'react'
+import theme from "@/config/theme";
+import { socket, SocketContext } from "@/context/SocketContext";
+import { UserContext } from "@/context/UserContext";
+import { User } from "@/type/User";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import type { AppProps } from "next/app";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [user, setUser] = useState<User>({
     username: "default username",
-    user_id: "default user_id"
-  })
+    user_id: "default user_id",
+  });
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-          <Component {...pageProps} />
-        </Box>
-      </ThemeProvider>
-    </UserContext.Provider>
-  )
+    <SocketContext.Provider value={socket}>
+      <UserContext.Provider value={{ user, setUser }}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+            <Component {...pageProps} />
+          </Box>
+        </ThemeProvider>
+      </UserContext.Provider>
+    </SocketContext.Provider>
+  );
 }
