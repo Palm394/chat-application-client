3609f8c02f23d1782a46c5cd989ef87d07517fdf
Chinnapat.N <69895436+Kkr1328@users.noreply.github.com>
2023-04-11 19:14:20 +0700

    Implement home service with fixed user id

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,48 @@ import CenterList from "@/component/chat/CenterList";
 import { Collapse } from "@mui/material";
 import useCollaspe from "@/hook/useCollaspe";
 import CollaspeButton from "@/module/home/CollaspeButton";
+import { useContext, useEffect, useState } from "react";
+import { SocketContext } from "@/context/SocketContext";
+import { UserContext } from "@/context/UserContext";
 
 export default function Home() {
-  const collaspeClient = useCollaspe()
-  const collaspeServer = useCollaspe()
+  const socket = useContext(SocketContext);
+  const userData = useContext(UserContext);
+
+  const collaspeClient = useCollaspe();
+  const collaspeServer = useCollaspe();
+
+  const [users, setUsers] = useState<any>({});
+  const [groups, setGroups] = useState<any>({});
+
+  useEffect(() => {
+    // retreive users
+    const userListener = (user: any) => {
+      setUsers((prevUsers: any) => {
+        const newUsers = { ...prevUsers };
+        newUsers[user._id] = user;
+        return newUsers;
+      });
+    };
+    socket.on("get_users_response", (res: any) => console.log(res.message));
+    socket.on("user", userListener);
+    // if (userData) {
+    socket.emit("getUsers", "64339a88ede9b0fc8a482d45");
+    // }
+    console.log(userData?.user);
+
+    // retreive group
+    const groupListener = (group: any) => {
+      setGroups((prevGroups: any) => {
+        const newGroups = { ...prevGroups };
+        newGroups[group._id] = group;
+        return newGroups;
+      });
+    };
+    socket.on("get_groups_response", (res: any) => console.log(res.message));
+    socket.on("group", groupListener);
+    socket.emit("getGroups");
+  }, [socket]);
 
   return (
     <>
@@ -16,36 +54,31 @@ export default function Home() {
       <CenterList>
         <CollaspeButton
           name={"Client"}
-          amount={0}
+          amount={[...Object.values(users)].length}
           onClick={collaspeClient.onClick}
         />
         <Collapse in={collaspeClient.open}>
-          <Chat href="/chat/1" label="client1" type="Direct" />
-          <Chat href="/chat/1" label="client1" type="Direct" />
-          <Chat href="/chat/1" label="client1" type="Direct" />
-          <Chat href="/chat/1" label="client1" type="Direct" />
-          <Chat href="/chat/1" label="client1" type="Direct" />
-        </Collapse >
+          {[...Object.values(users)].map((user: any, index: any) => (
+            <Chat
+              key={index}
+              href={`/group_chat/${user._id}`}
+              label={user.username}
+              type="Direct"
+            />
+          ))}
+        </Collapse>
         <CollaspeButton
           name={"Group"}
-          amount={0}
+          amount={[...Object.values(groups)].length}
           onClick={collaspeServer.onClick}
         />
         <Collapse in={collaspeServer.open}>
-          <Chat href="/group_chat/1" label="client1" type="Group" />
-          <Chat href="/group_chat/1" label="client1" type="Group" />
-          <Chat href="/group_chat/1" label="client1" type="Group" />
-          <Chat href="/group_chat/1" label="client1" type="Group" />
-          <Chat href="/group_chat/1" label="client1" type="Group" />
-          <Chat href="/group_chat/1" label="client1" type="Group" />
-          <Chat href="/group_chat/1" label="client1" type="Group" />
-          <Chat href="/group_chat/1" label="client1" type="Group" />
-          <Chat href="/group_chat/1" label="client1" type="Group" />
-          <Chat href="/group_chat/1" label="client1" type="Group" />
-          <Chat href="/group_chat/1" label="client1" type="Group" />
+          {[...Object.values(groups)].map((group: any, index: any) => (
+            <Chat key={index} href={`/group_chat/${group._id}`} label={group.name} type="Group" />
+          ))}
         </Collapse>
-      </CenterList >
+      </CenterList>
       <CreateGroup />
     </>
-  )
+  );
 }
