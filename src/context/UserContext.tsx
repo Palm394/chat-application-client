02901cca686025ab142af1f646c13eb0a8fe7b97
Chinnapat.N <69895436+Kkr1328@users.noreply.github.com>
2023-04-15 12:03:51 +0700

    Delete UserContext.tsx

--- src/context/UserContext.tsx
+++
@@ -1,9 +0,0 @@
-import { User } from "@/type/User";
-import { Dispatch, SetStateAction, createContext } from "react";
-
-type IUserContext = {
-  user: User;
-  setUser: Dispatch<SetStateAction<User>>;
-} | null;
-
-export const UserContext = createContext<IUserContext>(null)
