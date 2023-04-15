aa460169fd06c33583fc537aa9a77c84a5054dad
Chinnapat.N <69895436+Kkr1328@users.noreply.github.com>
2023-04-16 00:59:00 +0700

    Add send message service

--- src/component/chat/ChatBox.tsx
+++ src/component/chat/ChatBox.tsx
@@ -1,33 +1,51 @@
 import theme from "@/config/theme";
 import { IconButton, TextField as MuiTextField, styled } from "@mui/material";
-import SendIcon from '@mui/icons-material/Send';
-import { useState } from "react";
+import SendIcon from "@mui/icons-material/Send";
+import { useContext, useState } from "react";
 import withFooter from "@/hoc/Layout/withFooter";
 import { ChatType } from "@/type/Chat";
+import { SocketContext } from "@/context/SocketContext";
+import useLocalStorage from "@/hook/useLocalStorage";
+import { User } from "@/type/User";
 
 const TextField = styled(MuiTextField)({
-  '.MuiOutlinedInput-root': {
-    '&': {
-      borderColor: 'none',
+  ".MuiOutlinedInput-root": {
+    "&": {
+      borderColor: "none",
       backgroundColor: theme.palette.background.default,
-      borderRadius: '30px'
+      borderRadius: "30px",
     },
-    '&:hover': {
-      borderColor: 'none',
+    "&:hover": {
+      borderColor: "none",
     },
   },
 });
 
 type props = {
-  chatType: ChatType,
-  id: string
-}
+  chatType: ChatType;
+  id: string;
+};
 
 function ChatBox({ ...props }: props) {
-  const [value, setValue] = useState<string>("")
+  const socket = useContext(SocketContext);
+  const [currentUser, _] = useLocalStorage<User>("user_data");
+
+  const [value, setValue] = useState<string>("");
 
   function sendMessage() {
-    console.log(`Send ${props.chatType} message: ${props.id}`)
+    console.log(`Send ${props.chatType} message: ${props.id}`);
+
+    const messageInfo = {
+      type: props.chatType,
+      ownerId: currentUser.userId,
+      ownerName: currentUser.username,
+      ownerProfileImage: currentUser.profileImage,
+      chatId: props.id,
+      message: value,
+    };
+    socket.emit("createMessage", messageInfo);
+
+    setValue("");
   }
 
   return (
@@ -42,11 +60,10 @@ function ChatBox({ ...props }: props) {
           <IconButton onClick={sendMessage}>
             <SendIcon />
           </IconButton>
-        )
+        ),
       }}
     />
-  )
+  );
 }
 
-export default withFooter(ChatBox)
-
+export default withFooter(ChatBox);
