054e3acf7b59abd17f82ce5a8a51f26e5a67bbaa
Chinnapat.N <69895436+Kkr1328@users.noreply.github.com>
2023-04-16 00:55:13 +0700

    Add group name service

--- src/pages/group_chat/[group_id].tsx
+++ src/pages/group_chat/[group_id].tsx
@@ -7,6 +7,7 @@ import { SocketContext } from "@/context/SocketContext";
 import { useRouter } from "next/router";
 import useLocalStorage from "@/hook/useLocalStorage";
 import { User } from "@/type/User";
+import { GroupSocketType, MessageSocketType, ResType } from "@/type/Socket";
 
 export default function GroupChat() {
   const router = useRouter();
@@ -14,11 +15,12 @@ export default function GroupChat() {
   const [currentUser, _] = useLocalStorage<User>("user_data");
 
   const chatId = router.query.group_id?.toString().replaceAll('"', "");
-  const [messages, setMessages] = useState<any>({});
+  const [messages, setMessages] = useState<{ [key: string]: MessageSocketType }>({});
+  const [groupName, setGroupName] = useState<string>("");
 
-  useEffect(() => {
-    const messageListener = (message: any) => {
-      setMessages((prevMessages: any) => {
+  function getMessages() {
+    const messageListener = (message: MessageSocketType) => {
+      setMessages((prevMessages: { [key: string]: MessageSocketType }) => {
         const newMessages = { ...prevMessages };
         newMessages[message._id] = message;
         return newMessages;
@@ -27,46 +29,54 @@ export default function GroupChat() {
 
     const identifier = {
       type: "Group",
-      ownerId: currentUser.user_id,
+      ownerId: currentUser.userId,
       chatId: chatId,
     };
 
-    socket.on("get_messages_response", (res: any) => console.log(res.message));
+    socket.on("get_messages_response", (res: ResType) => console.log(res.message));
     socket.on("message", messageListener);
     socket.emit("getMessages", identifier);
+  }
+
+  function getGroupInformation() {
+    const groupListener = (group: GroupSocketType) => {
+      setGroupName(group.name);
+    };
+
+    socket.on("group", groupListener);
+    socket.on("get_group_by_id_response", (res: ResType) => console.log(res.message));
+    socket.emit("getGroupById", chatId);
+  }
+
+  useEffect(() => {
+    if (chatId) {
+      getMessages();
+      getGroupInformation();
+    }
   }, [socket, router]);
 
   return (
     <>
-      <NavBar
-        label={chatId}
-      />
+      <NavBar label={groupName} />
       <CenterList>
-        <Message
-          text={"TEMP OWN MESSAGE"}
-          isMine={true}
-          type={"Group"}
-        />
-        <Message
-          text={"TEMP MESSAGE"}
-          isMine={false}
-          type={"Group"}
-        />
         {[...Object.values(messages)]
-          .sort((a: any, b: any) => a.createdAt - b.createdAt)
-          .map((message: any) => (
+          .sort(
+            (a: MessageSocketType, b: MessageSocketType) =>
+              a.createdAt.valueOf() - b.createdAt.valueOf()
+          )
+          .map((message: MessageSocketType) => (
             <Message
               key={message._id}
               text={message.message}
               isMine={message.isOwner}
               type={"Group"}
+              senderName={message.username}
+              isLiked={message.isLiked}
+              totalLiked={message.like}
             />
           ))}
       </CenterList>
-      <ChatBox
-        chatType="Group"
-        id={chatId}
-      />
+      <ChatBox chatType="Group" id={chatId} />
     </>
   );
 }
