f91ae598888cb027302700ae06c3e8ac0a75ddd7
Chinnapat.N <69895436+Kkr1328@users.noreply.github.com>
2023-04-18 00:38:55 +0700

    Add update background image

--- src/component/NavBar/MenuNavBar.tsx
+++ src/component/NavBar/MenuNavBar.tsx
@@ -1,36 +1,51 @@
 import useModal from "@/hook/useModal";
 import { Button, MenuItem } from "@mui/material";
 import Dialog from "../common/Dialog";
-import Menu from "@/component/common/Menu"
+import Menu from "@/component/common/Menu";
 
-import CheckIcon from '@mui/icons-material/Check';
-import CloseIcon from '@mui/icons-material/Close';
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
+import { useContext } from "react";
+import { SocketContext } from "@/context/SocketContext";
 
 type props = {
-  open: boolean,
-  onClose: () => void
-}
+  open: boolean;
+  onClose: () => void;
+};
 
 export default function MenuChat({ ...props }: props) {
-  const modal = useModal()
+  const socket = useContext(SocketContext);
+
+  const modal = useModal();
+
+  function updateBackground() {
+    const updateInfo = {
+      type: "Group",
+      chatId: "64306c56162f6bf308503bd2",
+      backgroundImage: "",
+    };
+
+    socket.on("update_background_response", (res: any) => console.log(res.message));
+    socket.emit("updateBackground", updateInfo);
+  }
 
   return (
-    <Menu
-      open={props.open}
-      onClose={props.onClose}
-    >
+    <Menu open={props.open} onClose={props.onClose}>
       <MenuItem>
-        <Button onClick={modal.onOpen} sx={{ color: "black" }}>change background</Button>
+        <Button onClick={modal.onOpen} sx={{ color: "black" }}>
+          change background
+        </Button>
       </MenuItem>
       <Dialog
         open={modal.open}
         onClose={modal.onClose}
         header={"choose background image"}
-        content={
-          <>input image</>
-        }
-        iconAction={[[<CloseIcon />, modal.onClose], [<CheckIcon />, () => { }]]}
+        content={<>input image</>}
+        iconAction={[
+          [<CloseIcon />, modal.onClose],
+          [<CheckIcon />, () => {}],
+        ]}
       />
     </Menu>
-  )
+  );
 }
