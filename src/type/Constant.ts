f36cf8299daef5b43bad620341a93113faf7960a
Chinnapat.N <69895436+Kkr1328@users.noreply.github.com>
2023-04-18 20:25:30 +0700

    Add error socket message

--- src/type/Constant.ts
+++ src/type/Constant.ts
@@ -1,5 +1,7 @@
 export enum SOCKET_MESSAGE {
   SUCCESS = "Success",
+  USERNAME_IN_USE = "Username already in use",
+  NOT_FOUND = "Not found matching username and password",
 }
 
 export const DEFAULT_CURRENT_USER = {
