44701c1e715b60d330cf2580c1dee0b63faaeaba
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2023-04-18 14:30:08 +0700

    Validation for login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,7 +4,7 @@ import useLocalStorage from "@/hook/useLocalStorage";
 import { SOCKET_MESSAGE } from "@/type/Constant";
 import { ResType } from "@/type/Socket";
 import { User } from "@/type/User";
-import { Button, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
 
@@ -17,6 +17,7 @@ export default function Login() {
   const [password, setPassword] = useState<string>("");
   const [response, setResponse] = useState<ResType | null>(null);
 
+  const [isInputError, setIsInputError] = useState<boolean>(false)
   // open port to receive response from backend service
   socket.on("login_response", (res: ResType) => {
     setResponse(res);
@@ -39,22 +40,47 @@ export default function Login() {
   }, [socket, response]);
 
   function onSubmit(): void {
+    if (username.length === 0 || password.length === 0 || username.length > 20 || password.length < 6 || password.length >= 20) {
+      setIsInputError(true)
+      return
+    }
     socket.emit("login", { username: username, password: password });
     return;
   }
 
   return (
-    <>
-      <Typography>Login</Typography>
+    <Stack
+      sx={{
+        margin: "auto",
+      }}
+      spacing={3}
+    >
+      <Typography align="center">Login</Typography>
       <Stack sx={{ width: "300px" }} spacing={theme.spacing(2)}>
-        <Typography>Username</Typography>
-        <TextField value={username} onChange={(event) => setUsername(event.target.value)} />
-        <Typography>Password</Typography>
-        <TextField value={password} onChange={(event) => setPassword(event.target.value)} />
+        <Box sx={{
+          backgroundColor: "white",
+          border: "1px solid #000000",
+          borderRadius: "20px",
+          padding: "40px 20px"
+        }}>
+          <Typography align="center">nickname</Typography>
+          <TextField value={username} onChange={(event) => { setUsername(event.target.value); setIsInputError(false) }}
+            error={isInputError}
+            fullWidth />
+          <Typography align="center">password</Typography>
+          <TextField value={password} type={"password"} onChange={(event) => { setPassword(event.target.value); setIsInputError(false) }}
+            error={isInputError}
+            fullWidth
+          />
+          {isInputError &&
+            <Typography color={"error"} sx={{ marginTop: "30px" }} align="center">wrong nickname or password</Typography>
+          }
+        </Box>
+
         <Button onClick={onSubmit} variant="contained">
           Login
         </Button>
       </Stack>
-    </>
+    </Stack>
   );
 }
